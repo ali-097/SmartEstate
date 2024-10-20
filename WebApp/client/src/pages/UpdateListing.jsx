3f0815bf799df907d9f735805854ef1fb8271bee
ali-097 <aalimehmoodd@gmail.com>
2024-10-21 01:46:52 +0500

    Removed price limit form UpdateListing page

--- WebApp/client/src/pages/UpdateListing.jsx
+++ WebApp/client/src/pages/UpdateListing.jsx
@@ -490,7 +490,6 @@ export default function UpdateListing() {
                       type="number"
                       id="regularPrice"
                       min="50"
-                      max="10000000"
                       required
                       onChange={handleChange}
                       value={formData.regularPrice}
@@ -519,7 +518,7 @@ export default function UpdateListing() {
                       <div className="flex flex-col items-center">
                         <p>Discount price</p>
                         {formData.type === "rent" && (
-                          <span className="text-xs">($ / month)</span>
+                          <span className="text-xs">(Rs / month)</span>
                         )}
                       </div>
                     </div>
